55144f5c8cab4ab371be5d7bb21f51b14bfb37d4
Igor Bueno <ig.dsbueno@gmail.com>
2024-02-13 20:37:06 -0300

    Adding my first code

---
+++ BlockLetters.py
@@ -0,0 +1,11 @@
+iB= """       I I I I I     B B B B   
+           I         B      B
+           I         B      B
+           I         B B B B
+           I         B      B
+           I         B      B  
+       I I I I I     B B B B """ 
+print (iB)
+
+# My name is Igor Bueno
+# A fun fact about me is sometimes i I forget things
